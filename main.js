9ed8c8d9cc86787aacd71a8e3e015c4346cc170c
anepaz <anelisespaz@gmail.com>
2023-02-17 23:41:29 -0300

    create function to add new contact to form

---
+++ main.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form");
+const inputName = document.querySelector("input#name");
+const inputPhone = document.querySelector("input#phone");
+const button = document.querySelector("button");
+const table = document.querySelector("tbody");
+
+function addContact(name, phone) {
+  const tr = document.createElement("tr");
+  tr.innerHTML = `<td>${name}</td><td>${phone}</td>`;
+  table.appendChild(tr);
+}
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  addContact(inputName.value, inputPhone.value);
+
+  inputName.value = "";
+  inputPhone.value = "";
+});
