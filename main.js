2f5c2ddf003834118ddaf9e0ad9637a6b3220e03
anepaz <anelisespaz@gmail.com>
2023-10-23 22:02:58 -0300

    add strcuture to html, css and add jquery to project

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-const form = document.querySelector("form");
-const inputName = document.querySelector("input#name");
-const inputPhone = document.querySelector("input#phone");
-const button = document.querySelector("button");
-const table = document.querySelector("tbody");
-
-function addContact(name, phone) {
-  const tr = document.createElement("tr");
-  tr.innerHTML = `<td>${name}</td><td>${phone}</td>`;
-  table.appendChild(tr);
-}
-
-button.addEventListener("click", (e) => {
-  if (inputName.value && inputPhone.value) {
-    addContact(inputName.value, inputPhone.value);
-    inputName.value = "";
-    inputPhone.value = "";
-  }
-});
+$(document).ready(function() {
+  console.log(document.querySelector('header button'));
+  console.log($('#botao-cancelar'));
+})
